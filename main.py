258a176770bd014a6bdc96c4c4be1256ed9bc3e7
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2021-11-07 19:08:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,11 +21,10 @@ Triangle = """
   /               |
  /________________|
 C                  A
-
 """
 
-
-SideAsker = input("Combien de cotés connaissez vous ? 2/3")
+print('Tout dois etre en cm')
+SideAsker = input("Combien de cotés connaissez vous ? 2/3 ")
 if SideAsker == '1':
     print('Reponse non Valide')
     quit()
@@ -34,12 +33,11 @@ if SideAsker == '2':
     if Hypoténuse == "oui":
         print(Triangle)
         BC = input('BC = ')
-        WSide = input('Quelle coté connaissez vous ? BA ou CA ?')
+        WSide = input('Quelle coté connaissez vous ? BA ou CA ? ')
         if WSide == 'BA':
             BA = input('BA = ')
-            BCcarré = BC * BC 
+            AC = str(BC) * BC - BA * BA 
+            SecondSolve = AC ** (0.5)
+            print('AC = ', SecondSolve, 'cm')
         else:
             CA = input('CA = ')
-
-
-
